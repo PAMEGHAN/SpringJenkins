e3aa192e5d3d4a66fa2f8e99ba2a8db6ec93db21
pameghan <parepally.meghana@capgemini.com>
2022-06-29 15:09:01 +0530

    first commit

---
+++ src/test/java/com/javatechie/jenkin/api/SpringJenkinsApplicationTests.java
@@ -0,0 +1,24 @@
+package com.javatechie.jenkin.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@Runwith(SpringRunner.class)
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
+	@Test
+	public void contextLoads() {
+		logger.info("Test case executing...");
+		//logger.info("Test case executing second log statement...");
+		//logger.info("Test case executing third log statement...");
+		assertEquals(true,true);
+	}
+
+}
